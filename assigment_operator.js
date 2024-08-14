adaf36968107ad9aaf5510f18fdad0c6bc8588bd
parth5757 <psthakkar2@gmail.com>
2024-08-14 16:52:32 +0530

    js learning started

---
+++ assigment_operator.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 5;
+
+// initalize value
+console.log("Initalize Values")
+console.log("a = ", a)
+console.log("b = ", b)
+
+// assigment operator
+a = b;
+console.log("\nAfter 'a = b':");
+console.log("a =", a); // Output: a = 5
---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Welcome Parth")
+
+
+// Variables, Constant & Comments
+
+var score = 34; // "=" means assigment operator it assign the values
+var score = 53; // Redeclare Possible in var
+
+let points = 23;
+    
