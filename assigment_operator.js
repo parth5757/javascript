bc157794c84b7e705a35c31641ca145cbc3123bc
parth5757 <psthakkar2@gmail.com>
2024-08-14 19:32:05 +0530

    assigment perator are added

--- assigment_operator.js
+++ assigment_operator.js
@@ -1,12 +1,42 @@
 let a = 10;
 let b = 5;
 
-// initalize value
-console.log("Initalize Values")
-console.log("a = ", a)
-console.log("b = ", b)
+// // initalize value
+// console.log("Initalize Values")
+// console.log("a = ", a)
+// console.log("b = ", b)
 
-// assigment operator
-a = b;
-console.log("\nAfter 'a = b':");
-console.log("a =", a); // Output: a = 5
+// // assigment operator
+// a = b;
+// console.log("\nAfter 'a = b':");
+// console.log("a =", a); // Output: a = 5
+
+// // Addition assignment operator
+// a += 10;  // Equivalent to a = a + 5
+// console.log("\nAfter 'a += 5':");
+// console.log("a =", a); // Output: a = 10a
+
+// // Subtraction assignment operator
+// a -= 2;
+// console.log("\nAfter 'a -= 5':");
+// console.log("a = ", a)
+
+// // Multiplication assignment operator
+// a *= 2;  // Equivalent to a = a * 2
+// console.log("\nAfter 'a *= 2':");
+// console.log("a =", a); // Output: a = 20
+
+// // Division assignment operator
+// a /= 7;  // Equivalent to a = a / 7
+// console.log("\nAfter 'a /= 7':");
+// console.log("a =", a); // Output: a = 1.1428
+
+// // Modulus assignment operator
+// a %= 2;  // Equivalent to a = a % 2
+// console.log("\nAfter 'a %= 2':");
+// console.log("a =", a); // Output: a = 0
+
+// Exponentiation assignment operator
+b **= 3;  // Equivalent to b = b ** 3
+console.log("\nAfter 'b **= 3':");
+console.log("b =", b); // Output: b = 125
